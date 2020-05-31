3a71b9f0732952c963f8f54ae59261661030397d
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-31 15:50:18 +0900

    commit

--- env_error_tools/repair_mysql.sh
+++ env_error_tools/repair_mysql.sh
@@ -21,7 +21,7 @@ mysqlの修復を開始します。
 
 printf "
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-データベースのバックアップ作成を試みます。
+mysqlのバージョンをチェックします。
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 "
 
@@ -34,9 +34,13 @@ if [[ $MYSQL_VER == "" ]] ; then
   BREW_LIST_MYSQL=($(brew list | grep mysql@))
   if [[ $BREW_LIST_MYSQL[2] == "" ]] ; then
   MYSQL_VER=${BREW_LIST_MYSQL[1]##mysql@}
+  sed -i .bak '/opt\/mysql/d' ~/.$ENV_FILE
+  echo "export PATH="/usr/local/opt/mysql@${MYSQL_VER}/bin:""'$PATH' >> ~/.$ENV_FILE
+  source ~/.$ENV_FILE
   fi
 fi
 
+NO_MYSQL=false
 if [[ $MYSQL_VER == "5.6" ]] ; then
   cd .
 elif [[ $MYSQL_VER == "5.7" ]] ; then
@@ -45,70 +49,96 @@ else
   printf "\e[31m
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   mysqlに異常があります！
+  mysql5.6を入れ直します。
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   \e[m
   "
+  MYSQL_VER="5.6"
+  NO_MYSQL=true
 fi
 
-mysql.server stop
-printf "\e[31m
+if [[ $NO_MYSQL == "false" ]] ; then
+  printf "
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  データベースのバックアップ作成を試みます。
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  "
+
+  printf "\e[31m
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  パスワードの入力を求められたらMacのパスワードを入力してください。
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  \e[m
+  "
+  sudo chown -R "$(whoami)":admin /usr/local/var/mysql
+  sudo chmod 1777 /tmp
+  sudo chown root:wheel /tmp
+  sudo rm /tmp/mysql.sock
+  sudo pkill -kill -f mysql
+  mysql.server start
+  sleep 3
+  DB_BACKUP_NAME="`date "+%Y年%m月%d日%H時%M分%S秒"`_"MYSQL修復前"mysql@"$MYSQL_VER
+  DB_BACKUP_PATH="/Users/`whoami`/projects/env_error_questions/env_error_tools/db_backup/$DB_BACKUP_NAME.sql"
+  mkdir ~/projects/env_error_questions/env_error_tools/db_backup
+
+  mysqldump -u root --all-databases > $DB_BACKUP_PATH
+  : 'バックアップ作成失敗のとき'
+  if [ -z `cat $DB_BACKUP_PATH` ];then
+  printf "\e[31m
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  データベースのバックアップに失敗しました
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  \e[m
+  "
+  DB_BACKUP_SUCCESS=false
+  else
+  printf "\e[32m
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  バックアップの作成に成功しました。
+  ファイル名は
+
+  $DB_BACKUP_PATH
+
+  です。
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  \e[m
+  "
+  DB_BACKUP_SUCCESS=true
+  fi
+fi
+
+
+printf "\e[32m
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-途中でパスワードを求められたらMacのパスワードを入力してください。
+mysqlを全て削除します。
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 \e[m
 "
-sudo chown -R "$(whoami)":admin /usr/local/var/mysql
-sudo chmod 1777 /tmp
-sudo chown root:wheel /tmp
-sudo rm /tmp/mysql.sock
-sudo pkill -kill -f mysql
-mysql.server start
-sleep 3
-DB_BACKUP_NAME="`date "+%Y年%m月%d日%H時%M分%S秒"`_"MYSQL修復前"mysql@"$MYSQL_VER
-DB_BACKUP_PATH="/Users/`whoami`/projects/env_error_questions/env_error_tools/db_backup/$DB_BACKUP_NAME.sql"
-mkdir ~/projects/env_error_questions/env_error_tools/db_backup
-
-mysqldump -u root --all-databases > $DB_BACKUP_PATH
-: 'バックアップ作成失敗のとき'
-if [ -z `cat $DB_BACKUP_PATH` ];then
-printf "
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-データベースのバックアップに失敗しました
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-"
-DB_BACKUP_SUCCESS=false
-else
-printf "
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-バックアップの作成に成功しました。
-ファイル名は
-
-$DB_BACKUP_PATH
-
-です。
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-"
-DB_BACKUP_SUCCESS=true
-fi
-
-printf "\e[31m
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-mysqlを入れ直します。
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-\e[m
-"
-
 mysql.server stop
 brew uninstall mysql
 brew uninstall mysql@5.6
 brew uninstall mysql@5.7
+gem uninstall -aIx mysql2
 sudo rm -rf /usr/local/var/mysql
 sudo rm /etc/my.cnf
+printf "\e[32m
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+mysql$MYSQL_VERを入れ直します。
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+\e[m
+"
 brew install mysql@$MYSQL_VER
 
 sudo chown -R "$(whoami)":admin /usr/local/var/mysql
 sudo chmod 1777 /private/tmp
 sudo chown root:wheel /private/tmp
+
+printf "\e[32m
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+mysqlを起動します。
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+\e[m
+"
 
 sudo rm -rf /tmp/mysql.sock
 sudo pkill -KILL -f mysql
@@ -118,18 +148,20 @@ sleep 1
 mysql.server start
 
 if [[ $DB_BACKUP_SUCCESS = "true" ]] ; then
-  printf "
+  printf "\e[32m
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   バックアップからデータベースを復元します
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  \e[m
   "
 
   sleep 1
   mysql -u root < $DB_BACKUP_PATH
-  printf "
+  printf "\e[32m
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   データベースの復元が完了しました。
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  \e[m
   "
 fi
 
