2066e3101ecd3a2676417fb072f47ece54d12493
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-31 15:53:41 +0900

    Update mysql_checker.sh

--- env_error_tools/mysql_checker.sh
+++ env_error_tools/mysql_checker.sh
@@ -1,3 +1,17 @@
+EXIT=false
+printf "
+【shell】:チェック中...
+"
+source ~/projects/env_error_questions/env_error_tools/shell_checker.sh
+printf "
+【shell】:チェック完了
+"
+
+if [[ $EXIT == "true" ]] ; then
+  EXIT=false
+  return 2>&- || exit
+fi
+
 MYSQL_VER=""
 MYSQL_VER=`which mysql | grep mysql@`
 MYSQL_VER=`awk '{print substr($0, index($0, "mysql"))}' <<< $MYSQL_VER | awk '{sub("/bin.*", "");print $0;}'`
@@ -7,6 +21,9 @@ if [[ $MYSQL_VER == "" ]] ; then
   BREW_LIST_MYSQL=($(brew list | grep mysql@))
   if [[ $BREW_LIST_MYSQL[2] == "" ]] ; then
   MYSQL_VER=${BREW_LIST_MYSQL[1]##mysql@}
+  sed -i .bak '/opt\/mysql/d' ~/.$ENV_FILE
+  echo "export PATH="/usr/local/opt/mysql@${MYSQL_VER}/bin:""'$PATH' >> ~/.$ENV_FILE
+  source ~/.$ENV_FILE
   fi
 fi
 
